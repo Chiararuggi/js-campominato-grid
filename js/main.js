9d5254b01a8e92355ea69d3cba092251115ae578
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-07 17:40:59 +0100

    click + attempt to not print everytime a grid

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let cellDifficultyEasy= 100;
 let cellDifficultyMedium= 81;
 let cellDifficultyHard= 49;
-
+let tempCellAmount = 1;
 
 
 function createCell(cellAmount){
@@ -10,17 +10,24 @@ function createCell(cellAmount){
         let cell = createSquare(i);
         let grid = document.getElementById("grid");
         grid.appendChild(cell);
-    
+        tempCellAmount++;
     }
 }
 
 function createSquare(squareAmount){
     const cell = document.createElement("div");
     cell.classList.add("square");
+    cell.innerHTML += squareAmount;
+    cell.addEventListener("click", function() {
+
+        console.log("Clicked cell", squareAmount);
+    });
     return cell;
 }
 
 
 document.getElementById("playButton").addEventListener("click", function(){
-    createCell(cellDifficultyEasy);
+    if(tempCellAmount != cellDifficultyEasy)
+        createCell(cellDifficultyEasy);
+    
 });
