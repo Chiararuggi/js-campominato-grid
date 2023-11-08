825956d43a123ff5d8960e114d3602b4ea52f863
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-08 12:22:57 +0100

    fixed new grid

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 let cellDifficultyEasy= 100;
 let cellDifficultyMedium= 81;
 let cellDifficultyHard= 49;
-let tempCellAmount = 1;
 
 
 function createCell(cellAmount){
     
+    grid.innerHTML = "";
     for (let i = 1; i <= cellAmount; i++) {
         let cell = createSquare(i);
         let grid = document.getElementById("grid");
         grid.appendChild(cell);
-        tempCellAmount++;
     }
-}
+    
+};
 
 function createSquare(squareAmount){
     const cell = document.createElement("div");
@@ -23,11 +23,10 @@ function createSquare(squareAmount){
         console.log("Clicked cell", squareAmount);
     });
     return cell;
-}
+};
 
 
 document.getElementById("playButton").addEventListener("click", function(){
-    if(tempCellAmount != cellDifficultyEasy)
-        createCell(cellDifficultyEasy);
+    createCell(cellDifficultyEasy);
     
 });
